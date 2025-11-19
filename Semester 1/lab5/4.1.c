f3cfe56dd6c43e67c8d039edfe380352c0691847
DG <devyanshghanshala@gmail.com>
2025-11-20 01:52:15 +0530

    Create 4.1.c

---
+++ Semester 1/lab5/4.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+int main() { 
+    int n, pos = 0, neg = 0, zero = 0; 
+    char choice = 'y'; 
+    while (choice == 'y' || choice == 'Y') { 
+        printf("Enter a number: "); 
+        if (scanf("%d", &n) != 1) { 
+            return 0;  // Exit if invalid input 
+        } 
+        if (n > 0) 
+            pos++; 
+        else if (n < 0) 
+            neg++; 
+        else 
+            zero++; 
+        printf("Do you want to continue (y/n)? "); 
+        scanf(" %c", &choice); 
+    } 
+    printf("Positive numbers: %d\n", pos); 
+    printf("Negative numbers: %d\n", neg); 
+    printf("Zeros: %d\n", zero); 
+    return 0; 
+} 
