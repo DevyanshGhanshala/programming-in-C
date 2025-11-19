23917911a82e19ce0fa621fa4eb78d707881c13e
DG <devyanshghanshala@gmail.com>
2025-11-19 23:28:58 +0530

    Create 1.1.c

---
+++ Semester 1/lab1/1.1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  
+int main() { 
+printf("Name- Devyansh Ghanshala\nSAP ID - 590029047\nCourse- BCA\nBatch- 6\n");
+ printf("\n ---------- \n"); 
+printf("Hello World\n"); 
+return 0; 
+} 
